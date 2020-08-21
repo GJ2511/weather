9be3ac44394026519ec35a4edfbd010f6331c003
Gaurav Joshi <gaurav.joshi@nagarro.com>
2020-08-21 14:12:23 +0530

    Updates

---
+++ precache-manifest.d1209213fe78fa65f59c4e9b345ea9dc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94d637144b97ec7f2c8587eabe8ed395",
+    "url": "/weather/index.html"
+  },
+  {
+    "revision": "b9e900d6cc21005ced02",
+    "url": "/weather/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "16431d51dccba731cbff",
+    "url": "/weather/static/css/main.ed4dc794.chunk.css"
+  },
+  {
+    "revision": "b9e900d6cc21005ced02",
+    "url": "/weather/static/js/2.406618e0.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/weather/static/js/2.406618e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16431d51dccba731cbff",
+    "url": "/weather/static/js/main.0a59343c.chunk.js"
+  },
+  {
+    "revision": "65a135568c88d392bf25",
+    "url": "/weather/static/js/runtime-main.d8725fa2.js"
+  }
+]);
