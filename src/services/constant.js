b6df220ac5a461edf9592adb64688961d9332fec
Gaurav Joshi <gaurav.joshi@nagarro.com>
2020-08-26 17:42:57 +0530

    buils bug fix

--- src/services/constant.js
+++ src/services/constant.js
@@ -1,3 +1,3 @@
-export const API_URL = 'http://api.openweathermap.org';
+export const API_URL = 'https://api.openweathermap.org';
 export const WEATHER_URL = `${API_URL}/data/2.5/forecast`;
 export const API_KEY = 'ee84d7dc82a78d615bb769e17b0f594c';
